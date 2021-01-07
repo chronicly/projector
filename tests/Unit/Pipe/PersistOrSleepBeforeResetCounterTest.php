fd802961e94066e2b6129f3843297a1aa94816a9
steph bug <plexikongit@gmail.com>
2021-01-07 12:44:49 +0100

    tests

---
+++ tests/Unit/Pipe/PersistOrSleepBeforeResetCounterTest.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace Chronhub\Projector\Tests\Unit\Pipe;
+
+use Chronhub\Contracts\Projecting\EventCounter;
+use Chronhub\Contracts\Projecting\ProjectorContext;
+use Chronhub\Contracts\Projecting\ProjectorOption;
+use Chronhub\Contracts\Projecting\ProjectorRepository;
+use Chronhub\Projector\Pipe\PersistOrSleepBeforeResetCounter;
+use Chronhub\Projector\Tests\TestCaseWithProphecy;
+
+final class PersistOrSleepBeforeResetCounterTest extends TestCaseWithProphecy
+{
+    /**
+     * @test
+     */
+    public function it_sleep_before_update_lock(): void
+    {
+        // checkMe missing usleep test
+        $option = $this->prophesize(ProjectorOption::class);
+        $option->sleep()->willReturn(1000)->shouldBeCalled();
+
+        $eventCounter = $this->prophesize(EventCounter::class);
+        $eventCounter->reset()->shouldBeCalled();
+        $eventCounter->isReset()->willReturn(true)->shouldBeCalled();
+
+        $context = $this->prophesize(ProjectorContext::class);
+        $context->option()->willReturn($option->reveal());
+        $context->counter()->willReturn($eventCounter->reveal());
+        $context = $context->reveal();
+
+        $repository = $this->prophesize(ProjectorRepository::class);
+        $repository->updateLock()->shouldBeCalled();
+
+        $pipe = new PersistOrSleepBeforeResetCounter($repository->reveal());
+
+        $expectedContext = $pipe($context, function (ProjectorContext $next) {
+            return fn() => $next;
+        });
+
+        $this->assertEquals($expectedContext, fn() => $context);
+    }
+
+    /**
+     * @test
+     */
+    public function it_persist_before_reset_counter(): void
+    {
+        $option = $this->prophesize(ProjectorOption::class);
+        $option->sleep()->shouldNotBeCalled();
+
+        $eventCounter = $this->prophesize(EventCounter::class);
+        $eventCounter->reset()->shouldBeCalled();
+        $eventCounter->isReset()->willReturn(false)->shouldBeCalled();
+
+        $context = $this->prophesize(ProjectorContext::class);
+        $context->counter()->willReturn($eventCounter->reveal());
+        $context = $context->reveal();
+
+        $repository = $this->prophesize(ProjectorRepository::class);
+        $repository->persist()->shouldBeCalled();
+
+        $pipe = new PersistOrSleepBeforeResetCounter($repository->reveal());
+
+        $next = $pipe($context, function (ProjectorContext $context) {
+            return fn() => $context;
+        });
+
+        $this->assertEquals($next, fn() => $context);
+    }
+}
---
+++ tests/Unit/Pipe/PrepareQueryRunnerTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Chronhub\Projector\Tests\Unit\Pipe;
+
+use Chronhub\Contracts\Projecting\ProjectorContext;
+use Chronhub\Contracts\Projecting\StreamPosition;
+use Chronhub\Projector\Pipe\PrepareQueryRunner;
+use Chronhub\Projector\Tests\TestCaseWithProphecy;
+use ReflectionProperty;
+
+final class PrepareQueryRunnerTest extends TestCaseWithProphecy
+{
+    /**
+     * @test
+     */
+    public function it_prepare_streams_positions(): void
+    {
+        $streamPosition = $this->prophesize(StreamPosition::class);
+        $context = $this->prophesize(ProjectorContext::class);
+
+        $context->position()->willReturn($streamPosition);
+
+        $context->streamsNames()->willReturn(['foo', 'bar']);
+
+        $streamPosition->make(['foo', 'bar'])->shouldBeCalled();
+
+        $pipe = new PrepareQueryRunner();
+
+        $next = $pipe($context->reveal(), function (ProjectorContext $context) {
+            return fn() => $context;
+        });
+
+        $this->assertEquals($next, fn() => $context);
+    }
+
+    /**
+     * @test
+     */
+    public function it_prepare_streams_positions_once(): void
+    {
+        $context = $this->prophesize(ProjectorContext::class);
+        $context->position()->shouldNotBeCalled();
+
+        $pipe = new PrepareQueryRunner();
+
+        $ref = new ReflectionProperty($pipe, 'hasBeenPrepared');
+        $ref->setAccessible(true);
+        $ref->setValue($pipe, true);
+
+        $next = $pipe($context->reveal(), function (ProjectorContext $context) {
+            return fn() => $context;
+        });
+
+        $this->assertEquals($next, fn() => $context);
+    }
+}
