19e17af7b4925197775443daa659d8737b0bdb43
steph bug <plexikongit@gmail.com>
2021-04-19 05:30:08 +0200

    remove static methods from project facade

--- src/Support/Facade/Project.php
+++ src/Support/Facade/Project.php
@@ -3,10 +3,7 @@ declare(strict_types=1);
 
 namespace Chronhub\Projector\Support\Facade;
 
-use Chronhub\Contracts\Projecting\ProjectorFactory;
 use Chronhub\Contracts\Projecting\ProjectorManager;
-use Chronhub\Contracts\Projecting\ReadModel;
-use Chronhub\Contracts\Query\ProjectionQueryFilter;
 use Illuminate\Support\Facades\Facade;
 
 /**
@@ -17,51 +14,5 @@ final class Project extends Facade
     protected static function getFacadeAccessor(): string
     {
         return 'projector.manager';
-    }
-
-    public static function createQuery(string $name = 'default',
-                                       array $options = [],
-                                       ?ProjectionQueryFilter $queryFilter = null): ProjectorFactory
-    {
-        $projector = self::create($name);
-
-        return $projector
-            ->createQuery($options)
-            ->withQueryFilter(
-                $queryFilter ?? $projector->queryScope()->fromIncludedPosition()
-            );
-    }
-
-    public static function createProjection(string $streamName,
-                                            string $name = 'default',
-                                            array $options = [],
-                                            ?ProjectionQueryFilter $queryFilter = null): ProjectorFactory
-    {
-        $projector = self::create($name);
-
-        return $projector
-            ->createProjection($streamName, $options)
-            ->withQueryFilter(
-                $queryFilter ?? $projector->queryScope()->fromIncludedPosition()
-            );
-    }
-
-    public static function createReadModel(string $streamName,
-                                           string|ReadModel $readModel,
-                                           string $name = 'default',
-                                           array $options = [],
-                                           ?ProjectionQueryFilter $queryFilter = null): ProjectorFactory
-    {
-        $projector = self::create($name);
-
-        if (is_string($readModel)) {
-            $readModel = self::$app->make($readModel);
-        }
-
-        return $projector
-            ->createReadModelProjection($streamName, $readModel, $options)
-            ->withQueryFilter(
-                $queryFilter ?? $projector->queryScope()->fromIncludedPosition()
-            );
     }
 }
