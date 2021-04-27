ac185beb7f959c7762adbc4bff76d0176e9e5d91
steph bug <plexikongit@gmail.com>
2021-04-27 07:23:48 +0200

    minor

--- src/Concern/HasRemoteProjectionStatus.php
+++ src/Concern/HasRemoteProjectionStatus.php
@@ -15,15 +15,15 @@ trait HasRemoteProjectionStatus
     protected function stopOnLoadingRemoteStatus(bool $firstExecution, bool $keepRunning): bool
     {
         return match ($this->repository->loadStatus()) {
-            ProjectionStatus::STOPPING() => $this->stop($firstExecution),
-            ProjectionStatus::RESETTING() => $this->reset($firstExecution, $keepRunning),
-            ProjectionStatus::DELETING() => $this->delete($firstExecution, false),
-            ProjectionStatus::DELETING_EMITTED_EVENTS() => $this->delete($firstExecution, true),
+            ProjectionStatus::STOPPING() => $this->markAsStop($firstExecution),
+            ProjectionStatus::RESETTING() => $this->markAsReset($firstExecution, $keepRunning),
+            ProjectionStatus::DELETING() => $this->markAsDelete($firstExecution, false),
+            ProjectionStatus::DELETING_EMITTED_EVENTS() => $this->markAsDelete($firstExecution, true),
             default => false
         };
     }
 
-    private function stop(bool $firstExecution): bool
+    private function markAsStop(bool $firstExecution): bool
     {
         if ($firstExecution) {
             $this->repository->loadState();
@@ -34,7 +34,7 @@ trait HasRemoteProjectionStatus
         return $firstExecution;
     }
 
-    private function reset(bool $firstExecution, bool $keepRunning): bool
+    private function markAsReset(bool $firstExecution, bool $keepRunning): bool
     {
         $this->repository->reset();
 
@@ -45,7 +45,7 @@ trait HasRemoteProjectionStatus
         return false;
     }
 
-    private function delete(bool $firstExecution, bool $withEmittedEvents): bool
+    private function markAsDelete(bool $firstExecution, bool $withEmittedEvents): bool
     {
         $this->repository->delete($withEmittedEvents);
 
