63087646464f4d6484e8fdf6fb99dd2bcc84775d
steph bug <plexikongit@gmail.com>
2021-05-02 10:58:15 +0200

    remove useless doc

--- src/Pipe/StopWhenRunningOnce.php
+++ src/Pipe/StopWhenRunningOnce.php
@@ -14,8 +14,6 @@ final class StopWhenRunningOnce
 
     public function __invoke(ProjectorContext $context, callable $next): callable|bool
     {
-        // to be consistent when dispatching projector events
-        // we stop explicitly the projection when it's running once
         if (!$context->runner()->inBackground() && !$context->runner()->isStopped()) {
             $this->projector->stop();
         }
